085d994d5fdc5f2bd73c1d66d286e6e492c9ad7e
thenazareth <vitorianazareths@gmail.com>
2025-02-25 21:21:14 -0300

    little adjusts

--- HistorinhaOJogo/Assets/Script/Tutorial.cs
+++ HistorinhaOJogo/Assets/Script/Tutorial.cs
@@ -33,6 +33,12 @@ public class Tutorial : MonoBehaviour
     {
         if( espantalho.IsDestroyed() && etapaTutorial == 2)
         {
+            //se eu bater no espantalho andando, essa linhha trava meu controle 
+            //sobre o boneco, mas o boneco permanece no último estado de movimento
+            // que é andando, o que gera o bug de sair correndo do nada. Isso também
+            //faz ele mudar de cena antes de chamar o paladino o que gera o bug de
+            // gameobject destroyed, but still trying to access (o paladino é destruido
+            //quando muda de cena forçado pelo bug).
             player.GetComponent<Movimento>().podeMover = false;
             knightBattle1.Play();
         }
@@ -92,6 +98,7 @@ public class Tutorial : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D col)
     {
+        //Debug.Log(col.);
         if (col.gameObject.tag == "Player" && etapaTutorial == 1)   
         {
            battleTutorial.Play();
